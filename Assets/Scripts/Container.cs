116e12da7a21a4bc38073cbc9f0de1a3afeef8cb
ycMia <zzlyx20123201@126.com>
2020-10-03 13:22:18 +0800

    fc

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject[] container = new GameObject[100];
+    public float spawnBallSize;
+    public float moveAllowance;
+    public Vector3 spawnPointA;
+    public Vector3 spawnPointB;
+    public float SpawnBallSize { get => spawnBallSize; set => spawnBallSize = value; }
+
+    private float Vector3Modules(Vector3 v3)
+    {
+        return Mathf.Abs(Mathf.Pow(
+                      v3.x * v3.x
+                    + v3.y * v3.y
+                    + v3.z * v3.z, 0.5f));
+    }
+    private float Vector3Modules(Vector3 v3, Vector3 v3Reference)
+    {
+        return Mathf.Abs(Mathf.Pow(
+                      (v3.x - v3Reference.x) * (v3.x - v3Reference.x)
+                    + (v3.y - v3Reference.y) * (v3.y - v3Reference.y)
+                    + (v3.z - v3Reference.z) * (v3.z - v3Reference.z), 0.5f));
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int i = 0; i < container.Length; i++)
+        {
+            container[i] = Instantiate(prefab);
+            container[i].name = "Monoer"+i.ToString();
+            container[i].transform.parent = gameObject.transform;
+            container[i].GetComponent<Monoer>().ballSize = spawnBallSize + moveAllowance;
+            //container[i].transform.position = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+            //
+            Vector3 tposition;
+            float lengthabs;
+            if (Random.Range(0f, 1f) >= 0.5f)
+            {
+                do
+                {
+                    tposition = new Vector3(
+                        Random.Range(-spawnBallSize, spawnBallSize),
+                        Random.Range(-spawnBallSize, spawnBallSize),
+                        Random.Range(-spawnBallSize, spawnBallSize));
+                    tposition = new Vector3(tposition.x+spawnPointA.x,
+                        tposition.y + spawnPointA.y,
+                        tposition.z + spawnPointA.z);
+
+                    lengthabs = Vector3Modules(tposition,spawnPointA);
+            }
+                while (lengthabs >= spawnBallSize);
+            }
+
+            else
+            {
+                do
+                {
+                    tposition = new Vector3(
+                        Random.Range(-spawnBallSize, spawnBallSize),
+                        Random.Range(-spawnBallSize, spawnBallSize),
+                        Random.Range(-spawnBallSize, spawnBallSize));
+                    tposition += spawnPointB;
+
+                    lengthabs = Vector3Modules(tposition, spawnPointB);
+
+                }
+                while (lengthabs >= spawnBallSize);
+            }
+            container[i].transform.position = tposition;
+            ////
+            if (i == 0)
+            {
+                container[i].GetComponent<Monoer>().State = 2;//一个感染者
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Monoer.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Monoer : MonoBehaviour
+{
+    public float ballSize;
+    private int state = 0;
+    public int State { get => state; set => state = value; }
+
+    //0 health
+    //1 infected_incubation period
+    //2 infected
+
+    public Vector3 direction = new Vector3(0, 0, 0);
+    public float alphaValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+    //public LayerMask mask = 1<<8;
+    public void Infect()
+    {
+        float radius = 1.5f;
+        Collider[] cols = Physics.OverlapSphere(this.transform.position, radius/*, LayerMask.NameToLayer("layername")*/);
+        if (cols.Length > 0)
+        {
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if(cols[i].CompareTag("Wall"))
+                {
+                    break;
+                }
+                else if (cols[i].CompareTag("Monoer"))
+                {
+                    if(cols[i].gameObject.GetComponent<Monoer>().State != 2)
+                        cols[i].gameObject.GetComponent<Monoer>().State = 1;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+
+    private float Vector3Modules(Vector3 v3)
+    {
+        return Mathf.Pow(
+                      v3.x * v3.x
+                    + v3.y * v3.y
+                    + v3.z * v3.z, 0.5f);
+    }
+    private float Vector3Modules(Vector3 v3,Vector3 v3Reference)
+    {
+        return Mathf.Pow(
+                      (v3.x - v3Reference.x) * (v3.x - v3Reference.x)
+                    + (v3.x - v3Reference.y) * (v3.x - v3Reference.y)
+                    + (v3.x - v3Reference.z) * (v3.x - v3Reference.z), 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region
+        if (State == 0)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else if (State == 1)
+        {
+            //gameObject.GetComponent<Material>().color = Color.yellow;
+            State = 2;
+        }
+        else if (State == 2)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else
+        {
+            State = 0;
+        }
+        #endregion
+        //着色
+        if(State == 2)
+            Infect();
+
+        Vector3 t_vector = gameObject.GetComponent<Rigidbody>().velocity + gameObject.transform.position;
+        if(Vector3Modules(gameObject.transform.position) >= ballSize)
+        {
+            Vector3 n_vector = new Vector3(
+                -gameObject.transform.position.x / Vector3Modules(gameObject.transform.position),
+                -gameObject.transform.position.y / Vector3Modules(gameObject.transform.position),
+                -gameObject.transform.position.z / Vector3Modules(gameObject.transform.position));
+            gameObject.GetComponent<Rigidbody>().AddForce(n_vector);
+        }
+
+        else if (Vector3Modules(t_vector) >= ballSize)
+        {
+            Vector3 n_vector = new Vector3(
+                gameObject.transform.position.x / Vector3Modules(gameObject.transform.position),
+                gameObject.transform.position.y / Vector3Modules(gameObject.transform.position),
+                gameObject.transform.position.z / Vector3Modules(gameObject.transform.position));
+
+            Vector3 l_vector = gameObject.GetComponent<Rigidbody>().velocity;
+
+            gameObject.GetComponent<Rigidbody>().velocity = - 2 * (Vector3.Dot(l_vector, n_vector)) * n_vector + l_vector;
+        }
+
+        else
+        {
+            if (Random.Range(0f, 1f) <= 0.1f)
+            {
+                direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(direction.x * alphaValue, direction.y * alphaValue, direction.z * alphaValue));
+            }
+        }
+    }
+}
